63a5c38343d93eeca1996760e47630a17d93f6e5
spetk <spetk@SIMEON>
2024-09-14 01:01:00 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace HttpServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListeningSocket ListeningServer = new ListeningSocket();
+            ListeningServer.StartListeningSocket();
+        }
+    }
+}
